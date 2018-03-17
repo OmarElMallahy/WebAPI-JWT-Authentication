8935af6cffa4f1ea1f576aa0de56bec3c511057b
Omar <omar.elmallahy@gmail.com>
2018-03-17 16:43:00 +0200

    Initial commit

---
+++ JWTClient/JWTClient.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+
+namespace JWT.Client
+{
+    // This client is used to generate and validate JSON Web Tokens (JWT)
+    public class JWTClient
+    {
+        private const string Secret = "MyVerySecretSecretMyVerySecretSecretMyVerySecretMyVery";
+
+        
+        public static string GenerateToken(string nameClaim)
+        {
+            var symmetricKey = Convert.FromBase64String(Secret);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var now = DateTime.UtcNow;
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                        {
+                        new Claim(ClaimTypes.Name, nameClaim)
+                    }),
+
+                Expires = now.AddHours(Convert.ToInt32(ConfigurationManager.AppSettings["JWTAuthPeriodInHours"])),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            //TODO: Save token in redis or database
+
+            return token;
+        }
+        public static ClaimsPrincipal GetPrincipal(string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var jwtToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
+
+                if (jwtToken == null)
+                    return null;
+
+                var symmetricKey = Convert.FromBase64String(Secret);
+
+                var validationParameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(symmetricKey)
+                };
+
+                SecurityToken securityToken;
+                var principal = tokenHandler.ValidateToken(token, validationParameters, out securityToken);
+
+                return principal;
+            }
+
+            catch (Exception)
+            {
+                //should write log
+                return null;
+            }
+        }
+        public static bool IsValidToken(string token)
+        {
+            var simplePrinciple = GetPrincipal(token);
+            if (simplePrinciple == null)
+                return false;
+            var identity = simplePrinciple.Identity as ClaimsIdentity;
+
+            if (identity == null)
+                return false;
+
+            if (!identity.IsAuthenticated)
+                return false;
+
+            //TODO: Check if token exists and is valid from redis or database as an extra check
+            
+            return true;
+        }
+        
+
+
+    }
+}
---
+++ JWTModule/JWTModule.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using JWTClient;
+
+namespace JWTModule
+{
+    //This module is used in authenticating API requests using JWT
+    public class JWTModule : IHttpModule
+    {
+        public void Init(HttpApplication application)
+        {
+            application.BeginRequest +=
+                (new EventHandler(this.Application_BeginRequest));
+        }
+        private void Application_BeginRequest(Object source,
+         EventArgs e)
+        {
+
+            var request = HttpContext.Current.Request;
+
+            //Module functionality
+            var header = request.Headers.GetValues("Authorization");
+            string filePath = request.FilePath;
+            var nonAuthenticatedAPIs = ConfigurationManager.AppSettings["NonAuthenticatedAPIs"].Split(',');
+
+            if (request.HttpMethod == "OPTIONS")
+                return;
+            if (((filePath.StartsWith("/api")) ||(filePath.StartsWith("/Account"))) && (!(nonAuthenticatedAPIs.Any(a => filePath.Contains(a)&&!String.IsNullOrEmpty(a)))))
+            {
+
+                if (header == null)
+                {
+                    HttpContext.Current.Response.StatusCode = 401;
+                    return;
+                }
+                    
+                var token = header.FirstOrDefault();
+                if (String.IsNullOrEmpty(token))
+                {
+                    HttpContext.Current.Response.StatusCode = 401;
+                }
+                    
+
+
+
+                var isValidToken = JWTClient.IsValidToken(token);
+                if (!isValidToken)
+                {
+                    HttpContext.Current.Response.StatusCode = 401;
+                }
+                    
+            }
+            
+        }
+        
+        public void Dispose() { }
+    }
+}
